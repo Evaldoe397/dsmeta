91a703f1f80947b9283ffb62cd199e7dd66d2996
Evaldoe <evaldoe397@gmail.com>
2022-07-14 13:04:29 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-
+import NotificationButton from "./componets/NotificationButton"
+import './styles.css'
 
 function App() {
-  return(
-    <h1>Olá Mundo</h1>
+  return (
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+    </>
   )
 }
 
