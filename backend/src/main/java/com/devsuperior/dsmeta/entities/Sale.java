c56364acea76560bfcf6e076f3c00a0f9850fd98
Evaldoe <evaldoe397@gmail.com>
2022-07-16 21:11:12 -0300

    Mudando entities de Verbo para Sale

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_sales")
-public class Verbo {
+public class Sale {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +21,7 @@ public class Verbo {
 	private Double amount;
 	private LocalDate date;
 
-	public Verbo() {
+	public Sale() {
 
 	}
 
